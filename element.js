7e7b6e73adb6e03d1a7db38d909a2286184a9b5f
Igor Sergeev <igor.sergeev@tstllc.net>
2016-08-26 00:12:45 -0400

    Break down code into more files

---
+++ element.js
@@ -0,0 +1,85 @@
+
+
+function BuilderElement($rootElement, features) {
+	'use strict';
+	
+	this.checkFeatures(features)
+
+	this.$el = $('<div style="position: absolute; top: 10px; left: 10px; width: 150px; height: 90px; background-color: green;"></div>')
+	this.x   = 0 // Transient drag/move tracking...
+	this.y   = 0 // Transient drag/move tracking...
+
+	features.forEach(function(feature) {
+		this.$el.addClass(feature)
+	}, this)
+
+	this.installFeatures(features, this.$el)
+
+	$($rootElement).append(this.$el)
+}
+
+BuilderElement.prototype.supportedFeatures = [
+	'draggable',
+	'resizable'
+]
+
+BuilderElement.prototype.checkFeatures = function checkFeatures(features) {
+	features = typeof features === 'string'
+		? [features]
+		: features
+
+	if (!Array.isArray(features)) {
+		throw Error('Features argument should be an Array or string type')
+	}
+
+	features.forEach(function(feature) {
+		if (BuilderElement.prototype.supportedFeatures.indexOf(feature) < 0) {
+			throw Error('Feature "' + feature + '" is not supported"')
+		}
+	})
+}
+
+BuilderElement.prototype.installFeatures = function installFeatures(features) {
+	if (this.$el.hasClass('draggable')/* || $el.hasClass('resizable')*/) {
+		this.handleMouseMove = this.handleMouseMove.bind(this)
+		this.handleMouseDown = this.handleMouseDown.bind(this)
+		this.handleMouseUp   = this.handleMouseUp.bind(this)
+
+		this.$el.on('mousedown', this.handleMouseDown)
+		this.$el.on('mouseup',   this.handleMouseUp)
+	}
+}
+
+BuilderElement.prototype.handleMouseDown = function handleMouseDown(e) {
+	this.x = e.clientX
+	this.y = e.clientY
+
+	this.$el.on('mousemove', this.handleMouseMove)
+}
+
+BuilderElement.prototype.handleMouseUp = function handleMouseUp(e) {
+	this.x = 0
+	this.y = 0
+
+	this.$el.off('mousemove', this.handleMouseMove)
+}
+
+BuilderElement.prototype.handleMouseMove = function handleMouseMove(e) {
+	var offsetX = e.clientX - this.x
+	var offsetY = e.clientY - this.y
+
+	this.x = e.clientX
+	this.y = e.clientY
+
+	var elX  = parseInt(this.$el.css('left'))
+	var elY  = parseInt(this.$el.css('top'))
+
+	var newY = elY + offsetY
+	var newX = elX + offsetX
+
+	newY = newY < 0 ? 0 : newY
+	newX = newX < 0 ? 0 : newX
+
+	this.$el.css('top',  newY)
+	this.$el.css('left', newX)
+}
--- index.js
+++ index.js
@@ -1,108 +1,16 @@
 
-
-var builder = function(){
+var builder = function(rootElementSelector) {
 	'use strict';
 
-	var activeElement
+	var rootElement = $(rootElementSelector)
 
-	/**
-	 * Utitlities (BEGIN)
-	 */
-	 function findClosestOutside($elements, pointX, pointY) {
-		 console.log('pointY: ' + pointY)
-		 var boundElements = $elements.map(function(index, element) {
-			 var $element = $(element)
-			 return {
-				 el  : $element,
-				 rect: getRectangle($element)
-			 }
-		 })
-
-		 var outBoundElements = boundElements.filter(function(index, boundElement) {
-			 console.log(boundElement.rect.y1)
-			 //return (pointX < boundElement.rect.x1 || pointX > boundElement.rect.x2)
-			//  console.log(pointY)
-			//  console.log(boundElement.rect.y2)
-			 return (pointY < boundElement.rect.y1 || pointY > boundElement.rect.y2)
-					 //   && (pointY < boundElement.rect.y1 || pointY > boundElement.rect.y2)
-		 })
-
-		 console.log('outBoundElements: ' + outBoundElements.length)
-	 }
-
-	 function getRectangle($element) {
-		 var top  = parseInt($element.css('top'))
-		 var left = parseInt($element.css('left'))
-
-		 return {
-			 x1: left,
-			 y1: top,
-			 x2: left + $element.width(),
-			 y2: top  + $element.height()
-		 }
-	 }
-	 /**
- 	 * Utitlities (END)
- 	 */
-
-	registerEventListeners()
-
-	function addElement() {
-		var $el = $('<div class="draggable transformable" style="position: absolute; top: 10px; left: 10px; width: 150px; height: 90px; background-color: green;"></div>')
-		$('.builder').append($el)
-
-		$el.on('mousedown', function(e) { activeElement = $el    })
-		$el.on('mouseup'  , function(e) { activeElement = void 0 })
+	if (rootElement.length === 0) {
+		throw Error('No elements that match selector "' + rootElementSelector + '""')
 	}
 
-	function registerEventListeners() {
-		var $builderEl = $('.builder')
-		var x = 0
-		var y = 0
-
-		$('.add-element').on('click', addElement)
-
-		var dragMoveHandler = function (e) {
-			var offsetX = e.clientX - x
-			var offsetY = e.clientY - y
-
-			x = e.clientX
-			y = e.clientY
-
-			var elX  = parseInt(activeElement.css('left'))
-			var elY  = parseInt(activeElement.css('top'))
-
-			var newY = elY + offsetY
-			var newX = elX + offsetX
-
-			newY = newY < 0 ? 0 : newY
-			newX = newX < 0 ? 0 : newX
-
-			activeElement.css('top',  newY)
-			activeElement.css('left', newX)
+	return {
+		addElement: function addElement(features) {
+			var element = new BuilderElement(rootElement, features)
 		}
-
-		function rotateMoveHandler (e) {			
-			var transformables = $builderEl.find('.transformable')
-			findClosestOutside(transformables, e.clientX, e.clientY)
-		}
-
-		$builderEl.on('mousemove', rotateMoveHandler)
-
-		$builderEl.on('mousedown', function(e) {
-			if (activeElement) {
-				x = e.clientX
-				y = e.clientY
-
-				$builderEl.on('mousemove', dragMoveHandler)
-			}
-		})
-
-		$builderEl.on('mouseup', function(e) {
-			x = 0
-			y = 0
-
-			$builderEl.off('mousemove', dragMoveHandler)
-		})
 	}
 }
